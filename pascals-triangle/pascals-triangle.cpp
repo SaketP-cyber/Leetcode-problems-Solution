6e5b76a32d8d47746f82519525667f8b47d6718d
Saket Kumar Purwar <64796978+SaketP-cyber@users.noreply.github.com>
2021-11-10 20:17:43 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.80%) - LeetHub

---
+++ pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>>a(n);
+         for(int i=0;i<n;i++){
+            vector<int>b(i+1);
+            b[0]=1;
+            b[i]=1;
+            for(int j=1;j<i;j++){
+                b[j]=a[i-1][j]+a[i-1][j-1];        
+            }
+             a[i]=b;
+        }
+        return a;
+    }
+};
