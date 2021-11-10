a7c949ed66bb8cbc362461cb28fabdb85acf7fde
Saket Kumar Purwar <64796978+SaketP-cyber@users.noreply.github.com>
2021-11-10 20:22:18 +0530

    first Commit

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& a) {
-     int buy=INT_MAX,sell=0,ans=0;
-        buy=a[0];
+     int buy1=INT_MAX,sell=0,ans=0;
+        buy1=a[0];
         for(int i=1;i<a.size();i++){
-            ans=max(a[i]-buy,ans);
-            buy=min(a[i],buy);
+            ans=max(a[i]-buy1,ans);
+            buy1=min(a[i],buy);
         }
         return ans;
     }
