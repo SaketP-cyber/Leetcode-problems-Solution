cf0e0899734b3561050443f9827d14885800ba26
Saket Kumar Purwar <64796978+SaketP-cyber@users.noreply.github.com>
2021-11-17 11:46:18 +0530

    Time: 12 ms (78.02%), Space: 11 MB (25.23%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1,n1,n2;
- vector< int> v,nums2;
- nums2=nums;
- sort(nums2.begin(),nums2.end());
- while (low<high){
-	 if (nums2[low]+nums2[high]<target) {
-			 low++;
-		 }
-	 else if(nums2[low]+nums2[high]>target){
-			 high--;
-		 }
-	 else{   //nums2[low]+nums2[high]==target
-			 n1=nums2[low]; 
-			 n2=nums2[high];
-			 break;
- 		} 
- 	}
-	
-for (int i=0;i< nums.size() ; i++){
-         if (nums[i]==n1)
-         v.push_back(i);
-         else if (nums[i]==n2)
-         v.push_back(i);
-		 }
-		 return v;
+  set<int>s;
+        s.insert(nums[0]);
+        int num;
+        vector<int>ans;
+        for(int i=1;i<nums.size();i++){
+            if(s.count(target-nums[i])){
+                num=target-nums[i];
+                ans.push_back(i);
+                break;
+            }
+            s.insert(nums[i]);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==num){
+                ans.push_back(i);
+                break;
+            }
+        }
+        return ans;
 		 }
     
 };
