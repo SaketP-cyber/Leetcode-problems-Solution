015f1ef1ba23eeefdb7be052d395a24b21a72c01
Saket Kumar Purwar <64796978+SaketP-cyber@users.noreply.github.com>
2021-12-31 18:40:08 +0530

    Time: 24 ms (45.61%), Space: 14.8 MB (23.14%) - LeetHub

--- course-schedule/course-schedule.cpp
+++ course-schedule/course-schedule.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>a;
-        map<int,vector<int>>m1;
-        for(auto x:prerequisites){
-            m1[x[0]].push_back(x[1]);
-        }
-        
-        vector<int>indeg(numCourses,0);
-        for(auto x:m1){
-            for(auto j:x.second)
-            indeg[j]++;
-        }
-        queue<int>q;
-        for(int i=0;i<numCourses;i++){
-            if(indeg[i]==0)q.push(i);
-        }
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            a.push_back(node);
-            for(auto x:m1[node]){
-                indeg[x]--;
-                if(indeg[x]==0)q.push(x);
+    bool dfs(int node,map<int,vector<int>>&m1,vector<bool>&vis,vector<bool>&dfsVis){
+        vis[node]=1;
+        dfsVis[node]=1;
+        bool ans=1;
+        for(auto x:m1[node]){
+            if(vis[x] && dfsVis[x])ans= 0;
+            else if(vis[x]==0){
+                ans&=dfs(x,m1,vis,dfsVis);
             }
         }
-        if(a.size()==numCourses){
-            return 1;
+        dfsVis[node]=0;
+        return ans;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        map<int,vector<int>>m1;
+        for(int i=0;i<prerequisites.size();i++){
+            m1[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
-        return 0;
+        vector<bool>vis(numCourses,0),dfsVis(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                if(!dfs(i,m1,vis,dfsVis))return 0;
+            }
+        }
+        return 1;
     }
 };
