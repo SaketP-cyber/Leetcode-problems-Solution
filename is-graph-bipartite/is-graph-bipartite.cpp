ccbbfcd377f3538f0055bce8636304f63776af56
Saket Kumar Purwar <64796978+SaketP-cyber@users.noreply.github.com>
2022-01-02 13:00:54 +0530

    Time: 31 ms (27.04%), Space: 13.5 MB (80.64%) - LeetHub

--- is-graph-bipartite/is-graph-bipartite.cpp
+++ is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& m1) {
-        queue<int>q;
-        int n=m1.size();
-        vector<int>vis(n,0);
-        for(int i=0;i<m1.size();i++){
-            if(vis[i]==0){
-            vis[i]=501;
-            q.push(i);
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto x:m1[node]){
-                if(vis[x]==501 || vis[x]==502){
-                    if(vis[node]==vis[x])return 0;
+    bool dfs(int ind,vector<vector<int>>&m1,vector<int>&vis){
+        //vis[ind]=1;
+        bool ans=1;
+        for(auto x:m1[ind]){
+            if(vis[x]!=0){
+                if(vis[ind]==vis[x])ans=0;
+            }
+            else{
+                if(vis[ind]==501){
+                    vis[x]=502;
                 }
                 else{
-                    if(vis[node]==501){
-                        vis[x]=502;
-                    }
-                    else{
-                        vis[x]=501;
-                    }
-                    q.push(x);
+                    vis[x]=501;
                 }
+                ans&=dfs(x,m1,vis);
             }
         }
-        }
+        return ans;
+    }
+    bool isBipartite(vector<vector<int>>& m1) {
+        int n=m1.size();
+        vector<int>vis(n,0);
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                vis[i]=501;
+                if(!dfs(i,m1,vis)){
+                    return false;
+                }
+            }
         }
         return 1;
     }
